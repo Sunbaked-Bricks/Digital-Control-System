b34eb09b29a5da7c130b6832cd587b9bd0a4e511
Diego Ferrer <dferrer1@vols.utk.edu>
2023-03-07 14:23:33 -0500

    Added App Communication

--- wifi_webserver/wifi_webserver.ino
+++ wifi_webserver/wifi_webserver.ino
@@ -1,9 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <string>
+using namespace std;
 
 /* Put your SSID & Password */
 const char* ssid = "NodeMCU";  // Enter SSID here
-const char* password = "12345678";  //Enter Password here
+const char* password = "";  //Enter Password here
 
 /* Put IP Address details */
 IPAddress local_ip(192,168,1,1);
@@ -12,42 +14,58 @@ IPAddress subnet(255,255,255,0);
 
 ESP8266WebServer server(80);
 
-/* //v0
+// temperature (Farenheit)
+int cur_temp = 0;
+int tar_temp = 0;
+
+//*/ //v0
 uint8_t LED1pin = D7;
 bool LED1status = LOW;
 
 uint8_t LED2pin = D6;
 bool LED2status = LOW;
-*/
+//*/
 
 void setup() {
   Serial.begin(115200);
-  /* //v0
+  ///* //v0
+  // set pin modes for LEDs
   pinMode(LED1pin, OUTPUT);
   pinMode(LED2pin, OUTPUT);
-  */
+  //*/
 
+  // set wifi configuration
   WiFi.softAP(ssid, password);
   WiFi.softAPConfig(local_ip, gateway, subnet);
   delay(100);
 
   // web-server html pages
   server.on("/", handle_OnConnect);
-  /*
+  server.on("/info", handle_post);
+  server.on("/getTemp", handle_temp);
+  ///*
   // v0
   server.on("/led1on", handle_led1on);
   server.on("/led1off", handle_led1off);
   server.on("/led2on", handle_led2on);
   server.on("/led2off", handle_led2off);
-  */
+  //*/
+
+  // v1
+  server.on("/changeTarget", handle_changeTarget);
+  
   server.onNotFound(handle_NotFound);
   
   server.begin();
   Serial.println("HTTP server started");
 }
 void loop() {
-  server.handleClient();
-  /*
+  while(1) {
+    delay(10000);
+    server.handleClient();
+    Serial.println("test-remove later");
+  }
+  ///*
   // v0
   if(LED1status)
   {digitalWrite(LED1pin, HIGH);}
@@ -58,22 +76,35 @@ void loop() {
   {digitalWrite(LED2pin, HIGH);}
   else
   {digitalWrite(LED2pin, LOW);}
-  */
+  //*/
 }
 
+void handle_temp() {
+  Serial.println("temperature requested");
+  server.send(200, "text/plain", String(cur_temp));
+  cur_temp += 1;
+}
+void handle_post(){
+  String input = server.arg("plain");
+  Serial.println(input);
+  //Serial.println(server.arg("json"));
+  
+  server.send(200, "text/html", SendHomeHTML()); 
+}
 void handle_OnConnect() {
-  //LED1status = LOW;
-  //LED2status = LOW;
+  LED1status = LOW;
+  LED2status = LOW;
   Serial.println("GPIO7 Status: OFF | GPIO6 Status: OFF");
-  //server.send(200, "text/html", SendHTML(LED1status,LED2status)); //v0
-  server.send(200, "text/html", SendHTML()); //v1
+  server.send(200, "text/html", SendHTML(LED1status,LED2status)); //v0
+  //server.send(200, "text/html", SendHTML()); //v1
 }
 
-/* 
+///* 
 // v0
 void handle_led1on() {
   LED1status = HIGH;
   Serial.println("GPIO7 Status: ON");
+
   server.send(200, "text/html", SendHTML(true,LED2status)); 
 }
 
@@ -94,14 +125,19 @@ void handle_led2off() {
   Serial.println("GPIO6 Status: OFF");
   server.send(200, "text/html", SendHTML(LED1status,false)); 
 }
-*/
+//*/
+
+void handle_changeTarget() {
+  
+  server.send(200, "text/html", SendHomeHTML()); 
+}
 
 void handle_NotFound(){
   server.send(404, "text/plain", "Not found");
 }
 
-//String SendHTML(uint8_t led1stat,uint8_t led2stat){ //v0
-String SendHTML(){ //v1
+String SendHTML(uint8_t led1stat,uint8_t led2stat){ //v0
+//String SendHTML(){ //v1
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
   ptr +="<title>LED Control</title>\n";
@@ -119,8 +155,10 @@ String SendHTML(){ //v1
   ptr +="<h1>ESP8266 Web Server</h1>\n";
   ptr +="<h3>Using Access Point(AP) Mode</h3>\n";
 
-  /*
-  // v0
+  ptr +="<p>Target Temperature: " + String(tar_temp) + "</p>";
+  ptr +="<p>Current Temperature: " + String(cur_temp) + "</p>";
+  
+  ///* v0
   if(led1stat)
   {ptr +="<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";}
   else
@@ -130,7 +168,34 @@ String SendHTML(){ //v1
   {ptr +="<p>LED2 Status: ON</p><a class=\"button button-off\" href=\"/led2off\">OFF</a>\n";}
   else
   {ptr +="<p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a>\n";}
-  */
+  //*/
+
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
+
+String SendHomeHTML(){
+//String SendHTML(){ //v1
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>LED Control</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+  ptr +=".button-on {background-color: #1abc9c;}\n";
+  ptr +=".button-on:active {background-color: #16a085;}\n";
+  ptr +=".button-off {background-color: #34495e;}\n";
+  ptr +=".button-off:active {background-color: #2c3e50;}\n";
+  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<h1>ESP8266 Web Server</h1>\n";
+  ptr +="<h3>Using Access Point(AP) Mode</h3>\n";
+
+  ptr +="<p>Target Temperature: " + String(tar_temp) + "</p>";
+  ptr +="<p>Current Temperature: " + String(cur_temp) + "</p>";
 
   ptr +="</body>\n";
   ptr +="</html>\n";
