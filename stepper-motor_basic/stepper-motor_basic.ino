0509f63aea8d994b43e81f9e68c86f84ae90c67d
dferrer1 <47149079+dferrer1@users.noreply.github.com>
2023-03-19 23:34:50 -0400

    Added Stepper Motor Control

---
+++ stepper-motor_basic/stepper-motor_basic.ino
@@ -0,0 +1,174 @@
+#define IN1  D0
+#define IN2  D1
+#define IN3  D2
+#define IN4  D3
+#define CW   1
+#define CCW  0
+
+// motor variables
+int Steps = 0;
+int steps_left=50;
+int pos = -1;
+boolean Direction = true; //true -> clockwise, false -> counter-clockwise
+
+// time variables
+unsigned long last_time;
+unsigned long currentMillis;
+long tp;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(IN1, OUTPUT); 
+  pinMode(IN2, OUTPUT); 
+  pinMode(IN3, OUTPUT); 
+  pinMode(IN4, OUTPUT); 
+}
+
+void loop(){
+  step_once(CW);
+  delay(1000);
+}
+
+void step_once(int Direction) {
+  // handle direction of step
+  if (Direction == 1) {
+    pos += 1;
+    if (pos > 7) pos = 0;
+  } else if (Direction == 0) {
+    pos -= 1;
+    if (pos < 0) pos = 7;
+  } else {
+    Serial.print("ERROR: Invalid Turn Direction ["); Serial.print(Direction); Serial.println("]");
+  }
+
+  // step
+  switch(pos){
+   case 0:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, HIGH);
+   break; 
+   case 1:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, HIGH);
+     digitalWrite(IN4, HIGH);
+   break; 
+   case 2:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, HIGH);
+     digitalWrite(IN4, LOW);
+   break; 
+   case 3:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, HIGH);
+     digitalWrite(IN3, HIGH);
+     digitalWrite(IN4, LOW);
+   break; 
+   case 4:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, HIGH);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, LOW);
+   break; 
+   case 5:
+     digitalWrite(IN1, HIGH); 
+     digitalWrite(IN2, HIGH);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, LOW);
+   break; 
+     case 6:
+     digitalWrite(IN1, HIGH); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, LOW);
+   break; 
+   case 7:
+     digitalWrite(IN1, HIGH); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, HIGH);
+   break; 
+   default:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, LOW);
+   break; 
+  }
+}
+
+void stepper(int xw){
+  for (int x=0;x<xw;x++){
+    delay(500);
+    switch(Steps){
+       case 0:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, HIGH);
+       break; 
+       case 1:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, HIGH);
+       break; 
+       case 2:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 3:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 4:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 5:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+         case 6:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 7:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, HIGH);
+       break; 
+       default:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+    }
+  SetDirection();
+  }
+} 
+void SetDirection(){
+  Steps--;
+//if(Direction==true){ 
+// if(Steps  
+//Steps++;}
+//if(Direction==false){ Steps--; }
+//if(Steps>7){Steps=0;}
+if(Steps<0){Steps=7; }
+}
